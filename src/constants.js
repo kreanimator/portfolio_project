289f65915f23fb6f229a704f304e9172c373892e
kreanimator <backin.v@yandex.ru>
2024-07-11 00:02:17 +0300

    Finished main game functionality

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,25 @@
 export const scaleFactor = 4;
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Python and JavaScript these days and using serverless solutions of AWS Services.
+    I also like Java and TypeScript. Anyway regardless of the language, I just like programming.
+    Furthermore in my spare time I create videogames by using Godot and GDScript, but as an experiment I have projects fully written
+    in Java, Python, and JavaScript.
+    Here is my <a href="https://github.com/kreanimator" target="_blank">Github</a>!`,
+  diploma: `This is my MSc degree. It was in economics but I think it gave me a good foundation.`,
+  sofa_1: `That's my sofa. I like to relax here and watch YouTube or Netflix.`,
+  sofa_2: `I have a big sofa because I love to meet with my friends and colleagues.`,
+  tv: `That's my TV. I've been watching tech YouTubers a lot recently like:
+   <a href="https://www.youtube.com/@pixegami" target="_blank">pixegami</a>, <a href="https://www.youtube.com/@freecodecamp" target="_blank">freecodecamp</a>,
+  <a href="https://www.youtube.com/@BroCodez" target="_blank">BroCodez</a>!`,
+  bed: `This is my thinking chamber, also known as my bed. The best ideas hit me here,
+   usually right when I'm about to sleep. If I don't jot them down,
+   they'll keep me up all night doing mental gymnastics!`,
+
+  cv: `This is my desk and on it is my CV. <a href="" target="_blank">Check it out?</a>
+  Contact me at backin.v@gmail.com if you have any interesting job opportunities!`,
+    bookshelf: `Behold my mighty bookshelf, stacked with programming books I ambitiously purchased.
+    There's even one in French (oui, je parle francais et hebreu too!). Fun fact: I've read exactly one.
+     Anyone else treat tech books like Pokemon cards? Gotta buy 'em all, never read 'em all!`,
+  frog: `Wow! Is it a frog out there?`,
+  exit: `If you want to exit kreanimator's portfolio, just close the tab.`,
+};
