307620711027e27481126c9e8e8bb34f855c6e20
kreanimator <backin.v@yandex.ru>
2024-07-10 22:06:22 +0300

    Initialized player

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import { scaleFactor } from "./constants";
 import  { k } from "./kaboomCtx";
+
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
     sliceY: 31,
@@ -22,9 +24,42 @@ k.scene("main", async () =>  {
     const map = k.make([
         k.sprite("map"),
         k.pos(0),
-
+        k.scale(scaleFactor)
     ])
+    const player = k.make([
+        k.sprite("spritesheet",
+        {anim: "idle-down"}
+    ),
+        k.area({
+            shape: new k.Rect(k.vec2(0, 3), 10, 10 ),
+        }),
+        k.body(),
+        k.anchor("center"),
+        k.pos(),
+        k.scale(scaleFactor),
+        {
+            speed: 250,
+            direction: "down",
+            isInDialogue: false,
+        },
+        "player",
+    ]);
 
+    for (const layer of layers) {
+        if(layer.name === "boundaries"){
+            for (const boundary of layer.objects){
+                map.add([
+                    k.area({
+                        shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+                    }),
+                    k.body ({isStatic: true}),
+                    k.pos(boundary.x, boundary.y),
+                    boundary.name,
+                ])
+
+            }
+        }
+    }
 });
 
 
